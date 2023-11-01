ab7411c6f2bbdf72f8795e3e0cba9e53a844253d
Andre S Nascimento <andresoaresnascimento@gmail.com>
2023-10-31 21:26:38 -0300

    Primeiro Commit

---
+++ src/main/java/dev/snascimento/cnabbackend/CnabBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.snascimento.cnabbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnabBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnabBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/snascimento/cnabbackend/CnabBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.snascimento.cnabbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnabBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
